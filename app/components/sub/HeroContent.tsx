49d22fed8776b056f7ef750a54eb95d32c5a67e2
pierreviences <imaniyazid@gmail.com>
2024-02-13 13:45:31 +0700

    style: add ui hero section finished

--- app/components/sub/HeroContent.tsx
+++ app/components/sub/HeroContent.tsx
@@ -3,7 +3,12 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { SparklesIcon } from "@heroicons/react/24/solid";
-import { slideInFromLeft, slideInFromTop } from "@/app/utils/motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/app/utils/motion";
+import Image from "next/image";
 
 function HeroContent() {
   return (
@@ -15,7 +20,7 @@ function HeroContent() {
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5 " />
           <h1 className="Welcome-text text-[13px]">
@@ -25,15 +30,42 @@ function HeroContent() {
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6  mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6  mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
-          Providing
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 ">
-            the best
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 ">
+              {" "}
+              the best{" "}
+            </span>
+            Project Experience
           </span>
-          Project Experience
         </motion.div>
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I&apos;m Full Stack Developer Engineer with experience in Website,
+          Mobile, and Software Development. Check out my project and skills.
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
       </div>
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
     </motion.div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <main className="h-full w-full">
       <div className="flex flex-col h-[850px] gap-20">
         <Hero />
-        <h1 className="text-white">asd</h1>
       </div>
     </main>
   );
